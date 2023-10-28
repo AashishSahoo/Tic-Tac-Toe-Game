db67df0997db2844fa32781fe6428c6fd5ab91c4
ashish sahoo <ashishsahoo0013@gmail.com>
2023-10-28 16:50:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TicTacToe from './Component/TicTacToe/TicTacToe';
+import circle_icon from './Component/Assets/circle.png';
+import cross_icon from './Component/Assets/cross.png';
+function App() {
+  return (
+    <div className="App">
+        <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
